ac5e62801e8156c6f14be75eb4509cb8005e683c
Oleksandr Krysiuk <oleksandr.krysiuk@gmail.com>
2018-02-06 17:14:23 +0200

    saved

--- app36.js
+++ app36.js
@@ -1,2 +1,14 @@
 //Object.prototype
 //Person.prototype
+
+//Person constructor
+function Person(firstName, lastName, dob) {
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.birthday = new Date(dob);
+  this.calculateAge = function() {
+    const diff = Date.now() - this.birthday.getTime();
+    const ageDate = new Date(diff);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
+  };
+}
